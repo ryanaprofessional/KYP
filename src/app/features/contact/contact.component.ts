ec1a5ba2faa579a5386a7f044947d7275f037643
ryanaprofessional <ryan.m.anderson55@gmail.com>
2024-12-23 07:57:04 -0500

    Built base component for contact us page

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,11 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'kyp-contact',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  items = [{contactType: 'Email', contactDetails: 'email@email.com'}, {contactType: 'Phone', contactDetails: '888-111-1111'}]
 }
