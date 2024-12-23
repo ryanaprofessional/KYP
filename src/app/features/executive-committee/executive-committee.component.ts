afb2ff7e3a7d82936bbdb80024ec07fd536066eb
ryanaprofessional <ryan.m.anderson55@gmail.com>
2024-12-23 08:44:39 -0500

    Updated the executive committee component to use new cards

--- src/app/features/executive-committee/executive-committee.component.ts
+++ src/app/features/executive-committee/executive-committee.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../common/card/card.component';
+import { CommonModule } from '@angular/common';
+import { SubHeadingComponent } from '../common/sub-heading/sub-heading.component';
 
 @Component({
   selector: 'kyp-executive-committee',
-  imports: [],
+  imports: [CardComponent, CommonModule, SubHeadingComponent],
   templateUrl: './executive-committee.component.html',
   styleUrl: './executive-committee.component.scss'
 })
 export class ExecutiveCommitteeComponent {
-
+ items = [{name: 'Geoff Young', content: 'Chair', imageUrl: ''},
+  {name: 'Evy Tomboly', content: 'Vice Chair', imageUrl: ''},
+  {name: 'Geoff Young', content: 'Treasurer', imageUrl: ''},
+  {name: 'Ryan Anderson', content: 'Secretary', imageUrl: ''},
+  {name: 'Shauna Rudd', content: 'Committee Member', imageUrl: ''},
+  {name: 'Mikael Malone', content: 'Committee Member', imageUrl: ''}
+ ]
 }
