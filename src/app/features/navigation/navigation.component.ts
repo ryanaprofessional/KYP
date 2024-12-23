eab2880ccab94313dd8a9db023420f8ee14de585
ryanaprofessional <ryan.m.anderson55@gmail.com>
2024-12-22 22:11:10 -0500

    wip

--- src/app/features/navigation/navigation.component.ts
+++ src/app/features/navigation/navigation.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
 
 
 @Component({
   selector: 'kyp-navigation',
   imports: [MatToolbarModule,
     MatButtonModule,
-  RouterModule],
+  RouterModule, MatMenuModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
   standalone: true
