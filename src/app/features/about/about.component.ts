5a0e8bed814dd16c55c5bc49b2381516857c5323
ryanaprofessional <ryan.m.anderson55@gmail.com>
2024-12-23 08:44:53 -0500

    Imported the executive committee component to the about component

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PageHeaderComponent } from '../common/page-header/page-header.component';
+import { ExecutiveCommitteeComponent } from '../executive-committee/executive-committee.component';
 
 @Component({
   selector: 'kyp-about',
-  imports: [PageHeaderComponent],
+  imports: [PageHeaderComponent, ExecutiveCommitteeComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
