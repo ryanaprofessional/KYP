4382fe51a63f0933e9120cc4067732e08f364cb9
ryanaprofessional <ryan.m.anderson55@gmail.com>
2024-12-22 21:00:31 -0500

    Added router hookups to navigation bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './features/about/about.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { EventsComponent } from './features/events/events.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/about', pathMatch: 'full' },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'events', component: EventsComponent }
 ];
--- src/app/features/navigation/navigation.component.ts
+++ src/app/features/navigation/navigation.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'kyp-navigation',
-  imports: [],
+  imports: [MatToolbarModule,
+    MatButtonModule,
+  RouterModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
   standalone: true
