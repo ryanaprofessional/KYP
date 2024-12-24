15b19b2023f539c8343bbdda22dac28ee065268b
ryanaprofessional <ryan.m.anderson55@gmail.com>
2024-12-23 21:39:19 -0500

    Added Vision Committee Component to the About Page

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { PageHeaderComponent } from '../common/page-header/page-header.component';
 import { ExecutiveCommitteeComponent } from '../executive-committee/executive-committee.component';
+import { SubHeadingComponent } from '../common/sub-heading/sub-heading.component';
+import { VisionCommitteeComponent } from '../vision-committee/vision-committee.component';
 
 @Component({
   selector: 'kyp-about',
-  imports: [PageHeaderComponent, ExecutiveCommitteeComponent],
+  imports: [PageHeaderComponent, ExecutiveCommitteeComponent, SubHeadingComponent, VisionCommitteeComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
