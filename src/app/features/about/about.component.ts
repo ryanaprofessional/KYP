6a1df451d186ad61d6351683d051f70d15805ff3
ryanaprofessional <ryan.m.anderson55@gmail.com>
2024-12-23 22:08:13 -0500

    Laid some groundwork for further development of the bylaws shown on the about page

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -3,10 +3,11 @@ import { PageHeaderComponent } from '../common/page-header/page-header.component
 import { ExecutiveCommitteeComponent } from '../executive-committee/executive-committee.component';
 import { SubHeadingComponent } from '../common/sub-heading/sub-heading.component';
 import { VisionCommitteeComponent } from '../vision-committee/vision-committee.component';
+import { DividerComponent } from '../common/divider/divider.component';
 
 @Component({
   selector: 'kyp-about',
-  imports: [PageHeaderComponent, ExecutiveCommitteeComponent, SubHeadingComponent, VisionCommitteeComponent],
+  imports: [PageHeaderComponent, ExecutiveCommitteeComponent, SubHeadingComponent, VisionCommitteeComponent, DividerComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
