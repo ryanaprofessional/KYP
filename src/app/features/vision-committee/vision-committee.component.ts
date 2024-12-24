2002b92c69f4124c90c2160605ccb42ffab8f63d
ryanaprofessional <ryan.m.anderson55@gmail.com>
2024-12-23 21:39:05 -0500

    Created Vision Committee Component

--- src/app/features/vision-committee/vision-committee.component.ts
+++ src/app/features/vision-committee/vision-committee.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../common/card/card.component';
+import { CommonModule } from '@angular/common';
+import { SubHeadingComponent } from '../common/sub-heading/sub-heading.component';
 
 @Component({
   selector: 'kyp-vision-committee',
-  imports: [],
+  imports: [CardComponent, CommonModule, SubHeadingComponent],
   templateUrl: './vision-committee.component.html',
   styleUrl: './vision-committee.component.scss'
 })
 export class VisionCommitteeComponent {
-
+  items = [{name: 'Chris Campbell', content: 'Chair', imageUrl: ''},
+    {name: 'Alexin Hunter', content: 'Vice Chair', imageUrl: ''}
+   ]
 }
