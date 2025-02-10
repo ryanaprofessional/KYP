655b8a40fb30d186c963b6b7a5b91d39617e7912
ryanaprofessional <ryan.m.anderson55@gmail.com>
2025-02-09 20:27:38 -0500

    Removed the unused navigation items

--- src/app/features/navigation/navigation.component.ts
+++ src/app/features/navigation/navigation.component.ts
@@ -21,8 +21,6 @@ export class NavigationComponent {
     { label: 'About', link: '/about' },
     { label: 'Contact', link: '/contact' },
     { label: 'Events', link: '/events' },
-    { label: 'Resources', link: '/resources'},
-    { label: 'Store', link: '/store' },
     { label: 'Donate', link: '/donate' },
   ];
 
