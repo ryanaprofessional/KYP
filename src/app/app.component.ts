a459f352dba38bdb8191b4bdb03704fc0a5758a8
ryanaprofessional <ryan.m.anderson55@gmail.com>
2025-05-04 20:42:19 -0400

    Updated modal to use different image on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './features/navigation/navigation.component';
-import { ModalComponent } from './features/common/modal/modal.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent, ModalComponent, CommonModule],
+  imports: [RouterOutlet, NavigationComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kyp';
+  showModal = true;
+
+  ngOnInit(): void {
+    document.body.classList.add('modal-open');
+  }
+
+  onCloseModal() {
+    this.showModal = false;
+    document.body.classList.remove('modal-open');
+  }
 }
