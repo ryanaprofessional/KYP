2eb72a08bac88ba58d74f5a6d62728e905373b72
ryanaprofessional <ryan.m.anderson55@gmail.com>
2025-05-04 20:14:13 -0400

    basic modal going on app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './features/navigation/navigation.component';
+import { ModalComponent } from './features/common/modal/modal.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, ModalComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
